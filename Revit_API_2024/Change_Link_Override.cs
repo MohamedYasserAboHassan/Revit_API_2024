7ddf89dd3f5f9ca215aaf19853a3caace88cb009
Mohamed Yasser <mohamed.yasser2311@gmail.com>
2023-04-11 23:33:48 +0200

    new

---
+++ Revit_API_2024/Change_Link_Override.cs
@@ -0,0 +1,80 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Try_Revit_2024
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class Change_Link_Override : IExternalCommand
+
+    {
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            Document doc = uidoc.Document;
+
+            try
+            {
+
+
+
+                using (Transaction tns = new Transaction(doc, "Change Link Override"))
+                {
+                    tns.Start();
+
+                    var links = new FilteredElementCollector(doc).OfClass(typeof(RevitLinkType)).ToElementIds();
+
+
+
+                    RevitLinkGraphicsSettings revit_link = new RevitLinkGraphicsSettings();
+                    revit_link.LinkVisibilityType = LinkVisibility.ByHostView;
+
+
+                    View Current_view = doc.ActiveView;
+
+
+                    foreach (var item in links)
+                    {
+
+                        ElementId id = new ElementId(item.Value);
+                        Current_view.SetLinkOverrides(id, revit_link);
+
+
+
+                    }
+
+                    tns.Commit();
+
+
+                    TaskDialog.Show("Override Links", "All Links are overrides");
+
+                }
+
+
+
+
+
+
+
+                return Result.Succeeded;
+
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+            }
+
+
+
+        }
+    }
+}
---
+++ Revit_API_2024/Get_Element_Parameters.cs
@@ -0,0 +1,72 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_Revit_2024
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class Get_Element_Parameters : IExternalCommand
+
+    {
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            Document doc = uidoc.Document;
+
+            try
+            {
+
+
+
+
+
+
+                var ref_element = uidoc.Selection.PickObject(ObjectType.Element);
+
+                var element = doc.GetElement(ref_element);
+
+                StringBuilder sb = new StringBuilder();
+                foreach (var item in element.EvaluateAllParameterValues())
+                {
+                    sb.AppendLine(item.Definition.Name + "=" + item.AsValueString(doc));
+
+                }
+
+                TaskDialog.Show("Parameters", sb.ToString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                return Result.Succeeded;
+
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+            }
+
+
+
+        }
+    }
+}
